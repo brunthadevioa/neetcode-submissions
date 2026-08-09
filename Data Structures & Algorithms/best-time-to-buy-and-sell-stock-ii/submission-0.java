9edf74110c9a9ee71ba080da6addd5d54e376da2
BRUNTHADEVI O A <brunthadevioa.24csd@kongu.edu>
2026-08-09 22:20:36 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int p = 0;
+
+        int n = prices.length;
+
+        for(int i=0;i<n-1;i++){
+
+            if(prices[i]<prices[i+1])
+
+                p+=prices[i+1]-prices[i];
+            
+        }
+
+        return p;
+        
+    }
+}
